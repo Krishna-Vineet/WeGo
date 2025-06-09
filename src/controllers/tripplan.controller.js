96564ccde666547ba2324320571fbb19e5229b62
Krishna-Vineet <your-email@domain.com>
2025-06-09 23:19:27 +0530

    issue resolved

--- src/controllers/tripplan.controller.js
+++ src/controllers/tripplan.controller.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiErrors.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import TripPlan from "../models/tripPlan.model.js";
+import TripPlan from "../models/tripplan.model.js";
 import User from "../models/user.model.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
